e2cb9290b78986012e4e917ea489aa654e707859
Scott Atkins <scottrick49@gmail.com>
2022-02-03 11:10:55 -0600

    Removed unused CardsConfig file.

--- swccg/src/main/java/com/hatfat/swccg/data/CardsConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.hatfat.swccg.data
-
-import java.io.Serializable
-
-data class CardsConfig(
-    /* PlayStore images are greatly reduced in resolution */
-    val shouldUsePlayStoreImages: Boolean = false
-) : Serializable
