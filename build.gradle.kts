653fc7ea7c256afc053c9d52edac23b19d1ebdf3
Scott Atkins <scottrick49@gmail.com>
2022-02-06 10:56:05 -0600

    Updated for latest Android Studio.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
     dependencies {
         // Don't put this in Dependencies, otherwise Android Studio has trouble automatic upgrades.
-        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("com.android.tools.build:gradle:7.1.1")
         classpath(BuildPlugins.kotlinGradlePlugin)
         classpath(BuildPlugins.navigationSafeArgsGradlePlugin)
         classpath(BuildPlugins.hiltGradlePlugin)
