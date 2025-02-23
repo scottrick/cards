e3eb50bb59cc867ae6fdeb3bd895cb665433fb22
Scott Atkins <scottrick49@gmail.com>
2025-02-23 11:31:59 -0600

    Added CardView which supports card 3d rotation.

---
+++ cards/src/main/java/com/hatfat/cards/util/CardView.kt
@@ -0,0 +1,120 @@
+package com.hatfat.cards.util
+
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.content.Context
+import android.graphics.Camera
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatImageView
+import kotlin.math.max
+import kotlin.math.min
+
+class CardView : AppCompatImageView {
+    private val camera: Camera = Camera()
+    private val matrix: Matrix = Matrix()
+
+    private var rotationX = 0f
+    private var rotationY = 0f
+
+    private var lastX = 0f
+    private var lastY = 0f
+
+    private var animator: ObjectAnimator? = null
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun setRotationY(rotationY: Float) {
+        this.rotationY = rotationY
+        invalidate()
+    }
+
+    override fun setRotationX(rotationX: Float) {
+        this.rotationX = rotationX
+        invalidate()
+    }
+
+    private fun startRotationBack() {
+        stopRotationBack()
+
+        val xPropertyHolder = PropertyValuesHolder.ofFloat("rotationX", rotationX, 0f)
+        val yPropertyHolder = PropertyValuesHolder.ofFloat("rotationY", rotationY, 0f)
+
+        val newAnimator =
+            ObjectAnimator.ofPropertyValuesHolder(this, xPropertyHolder, yPropertyHolder)
+
+        newAnimator.setDuration(300)
+        newAnimator.start()
+
+        animator = newAnimator
+    }
+
+    private fun stopRotationBack() {
+        animator?.cancel()
+        animator = null
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    stopRotationBack()
+                    lastX = event.x
+                    lastY = event.y
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    startRotationBack()
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    val changeX = event.x - lastX
+                    val changeY = event.y - lastY
+                    lastX = event.x
+                    lastY = event.y
+
+                    val scale = min(width, height) / 25f
+                    val rotationChangeX = changeY / -scale
+                    val rotationChangeY = changeX / scale
+                    rotationX += rotationChangeX
+                    rotationY += rotationChangeY
+
+                    val maxRotation = 6f
+                    rotationX = min(maxRotation, rotationX)
+                    rotationX = max(-maxRotation, rotationX)
+                    rotationY = min(maxRotation, rotationY)
+                    rotationY = max(-maxRotation, rotationY)
+
+                    invalidate()
+                }
+            }
+        }
+
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        camera.save()
+        camera.rotateX(rotationX)
+        camera.rotateY(rotationY)
+        camera.getMatrix(matrix)
+        camera.restore()
+
+        matrix.preTranslate(-width / 2f, -height / 2f)
+        matrix.postTranslate(width / 2f, height / 2f)
+
+        canvas.concat(matrix)
+
+        super.onDraw(canvas)
+    }
+}
